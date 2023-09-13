ecef387670e3a1edbda6b2295c9bc73194203772
Sebastian Hoffmann <shoffmann.git@gmail.com>
2023-09-13 18:06:47 +0200

    chore: formatting

--- test/test_sharding.py
+++ test/test_sharding.py
@@ -1,10 +1,9 @@
 import sys
 
 import numpy as np
-from numpy.testing import assert_array_equal
 import pytest
-
 from dmlcloud.util import shard_indices
+from numpy.testing import assert_array_equal
 
 
 class TestSharding:
