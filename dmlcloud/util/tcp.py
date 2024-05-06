ba8c74b556aca29bea65508c1642c74939293a83
Sebastian Hoffmann <shoffmann.git@gmail.com>
2024-05-06 19:15:26 +0200

    feat: more robust name resolution

--- dmlcloud/util/tcp.py
+++ dmlcloud/util/tcp.py
@@ -1,5 +1,5 @@
 import socket
-
+import subprocess
 
 def find_free_port():
     """
@@ -10,9 +10,17 @@ def find_free_port():
         return s.getsockname()[1]
 
 
-def get_local_ips():
+def get_local_ips(use_hostname=True):
     """
     Returns the IP addresses of the local machine.
     """
-    hostname = socket.gethostname()
-    return socket.gethostbyname_ex(hostname)[2]
+    if use_hostname:
+        proc = subprocess.run(['hostname', '-I'], capture_output=True, text=True)
+        if proc.returncode == 0:
+            return proc.stdout.strip().split(' ')
+        else:
+            err = proc.stderr.strip()
+            raise RuntimeError(err)
+    else:
+        hostname = socket.gethostname()
+        return socket.gethostbyname_ex(hostname)[2]
