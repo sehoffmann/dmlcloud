891f746bd21e9c66ea936a18861e0b53a8832fb6
Sebastian Hoffmann <shoffmann.git@gmail.com>
2025-04-29 16:49:42 +0200

    feat: truncate overly long git diffs

--- dmlcloud/core/callbacks/git.py
+++ dmlcloud/core/callbacks/git.py
@@ -9,6 +9,29 @@ class GitDiffCallback(Callback):
     A callback that prints a git diff and if checkpointing is enabled, saves it to the checkpoint directory.
     """
 
+    def __init__(self, max_chars=2500):
+        """
+        Args:
+            max_chars: The maximum number of characters to log to console. (default: 2500)
+        """
+        self.max_chars = max_chars
+
+    def _log_diff(self, diff):
+        truncate = self.max_chars and len(diff) > self.max_chars
+
+        if truncate:
+            msg = '* GIT-DIFF (truncated):\n'
+            lines = diff[: self.max_chars].splitlines()
+        else:
+            msg = '* GIT-DIFF:\n'
+            lines = diff.splitlines()
+
+        msg += '\n'.join('    ' + line for line in lines)
+        if truncate:
+            msg += f'\n    ... (truncated to {self.max_chars} characters, total: {len(diff)})'
+
+        dml_logging.info(msg)
+
     def pre_run(self, pipe):
         diff = git_diff()
         if diff is None:
@@ -17,9 +40,7 @@ class GitDiffCallback(Callback):
         if pipe.run_dir and is_root():
             self._save(pipe.run_dir / 'git_diff.txt', diff)
 
-        msg = '* GIT-DIFF:\n'
-        msg += '\n'.join('    ' + line for line in diff.splitlines())
-        dml_logging.info(msg)
+        self._log_diff(diff)
 
     def _save(self, path, diff):
         with open(path, 'w') as f:
